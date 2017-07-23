e1f953fdf6552396af6bd9b80b870c89b68a1c59
Jakob Krigovsky <jakob@krigovsky.com>
2017-07-23 23:30:17 +0200

    Prepare prompting and templating

--- app/index.js
+++ app/index.js
@@ -3,10 +3,20 @@
 const Generator = require('yeoman-generator')
 
 module.exports = class extends Generator {
+  prompting () {
+    const prompts = []
+
+    return this.prompt(prompts).then(answers => {
+      this.answers = answers
+    })
+  }
+
   writing () {
-    this.fs.copy(
+    this.fs.copyTpl(
       this.templatePath('*'),
       this.destinationPath(),
+      this.answers,
+      {},
       {
         globOptions: {
           dot: true
